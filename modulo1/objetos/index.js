e52bcd0253684a1fc5dedf9454186654dc6d3bfe
Antonio Marcos Dias Soares <atmarcos28@gmail.com>
2022-04-26 21:59:55 -0300

    Exercicio de terça 26.04

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,131 @@
+// EXERCICIO DE INTERPRETAÇÃO 
+
+
+// 1
+
+// A- Matheus Naschtergeale
+//    Virginia cavendish  - DUVIDA POR QUE ELE TA CONTANDO DENTRO DE UM ARRAY
+//   Globo 14H
+
+
+// 2
+
+// A -
+ // Juca,3 ,srd 
+ // juba,3,srd
+ //jubo ,3,srd
+
+
+
+// B - espalhamneto, ou copiar o obejto, um backup tbm.
+
+ 
+
+// 3
+
+/*  function minhaFuncao(objeto, propriedade) {
+	return objeto[propriedade]
+}
+
+const pessoa = {
+  nome: "Caio", 
+  idade: 23, 
+  backender: false
+}
+
+console.log(minhaFuncao(pessoa, "backender"))
+console.log(minhaFuncao(pessoa, "altura")) */  
+
+
+// A- false,undefined
+
+
+// B -  o False e por que e quis imprimir no meu console o que estava "armazenado" no meu backender,
+//    e o undefined por que nao tem altura nem na funcao nem no objeto
+
+
+
+
+// EXERCICIO DE ESCRITA
+
+// 1 
+
+
+/* const pessoa ={
+    nome:"Antonio",
+    apelidos:["toninho","teteu","toto"],
+
+}
+console.log("Eu sou(a)",pessoa.nome,"Mais pode me chamar de ",pessoa.apelidos[0],",",pessoa.apelidos[1],"e",pessoa.apelidos[2]);
+
+const pessoa2 ={
+    ... nome,
+    novosApelidos:["Gordo","chato","chefinho"],
+}
+console.log() */
+
+
+// 2 - EXECUTADO
+
+  /* const primeiraPessoa ={
+ nome:"bruna",
+ idade:20,
+ profissao:"medica"
+
+}
+
+const segundaPessoa ={
+    nome:"Jefferson",
+    idade:30,
+    profissao:"medico"
+}
+
+function receber(dados){
+   let funcao = (Array(dados.nome, dados.nome.length, dados.idade, dados.profissao, dados.profissao.length))
+   return console.log(funcao)
+}
+
+receber(primeiraPessoa)
+receber(segundaPessoa) */
+ 
+
+
+
+
+
+// - FALTO APENAS A QUANTIDADE DE CARACTERIS DA PROFISSAO
+
+
+// 3 - EXECUTADO
+
+ /*  let carrinho = []
+
+frutaVermelha={
+    nome:"maça",
+    disponibilidade:true
+
+}
+
+frutaVerde={
+    nome:"Kiwi",
+    disponibilidade1:true
+}
+
+frutaAmarela={
+    nome:"banana",
+    disponibilidade2:true
+
+}
+
+
+
+function adicionandoFrutas () {
+return frutaVermelha    
+
+    
+}
+
+carrinho = [adicionandoFrutas(frutaVermelha)]
+carrinho.push(frutaVerde,frutaAmarela)
+console.log(carrinho)
+ */
