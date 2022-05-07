a76d55718e36ae843bb0428e2ea9a1d5df84aa41
Antonio Marcos Dias Soares <atmarcos28@gmail.com>
2022-05-06 22:08:49 -0300

    Projeto Black-Jack 06.05

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,43 @@
+
+// CODIGO 
+
+
+
+
+
+/* console.log("Boas vindas ao jogo de Blackjack!")
+
+
+if(confirm("Vamos iniciar a Rodada ?")) { // SE O USUARIO COMECAR O JOGO 
+
+    console.log("BORA JOGARR!")  // SO PRA TER A CERTEZA DE QUE IA ENTRAR O CONFIRM
+     let cartao1antoio = comprarCarta()
+     let carta2antonio = comprarCarta()
+     let carta1pc = comprarCarta()
+     let carta2pc = comprarCarta()
+
+       
+     let pontuacaoAntonio  =Number ( cartao1antoio.valor + carta2antonio.valor)
+     let pontuacaopc  = Number(carta1pc.valor + carta2pc.valor)
+     
+     console.log(`Cartas Antonio ${cartao1antoio.texto} ${carta2antonio.texto} - Pontuação :${pontuacaoAntonio}`)
+     console.log(`Cartas Pc ${carta1pc.texto} ${carta2pc.texto} - Pontuação :${pontuacaopc}`)
+    
+      if (pontuacaoAntonio > pontuacaopc){
+          console.log("Antonio GANHOU!!!")
+         }
+
+      else if (pontuacaopc > pontuacaoAntonio){
+          console.log("Pc GANHOU!!!")
+         }
+      else if(pontuacaoAntonio === pontuacaopc){
+          console.log("EMPATOU!!!")
+         }
+    
+    
+} else {
+    console.log("O Jogo Acabou!") // o que fazer se o usuário clicar "cancelar"
+    
+}
+ */
+ 
